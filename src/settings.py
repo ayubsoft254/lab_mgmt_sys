f85b3df7213b95631864ec7dceadc75f99a4f63b
Ayub Henry <110997339+ayubsoft254@users.noreply.github.com>
2025-05-14 17:33:54 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -41,7 +41,7 @@ if all([ADMIN_USER_NAME, ADMIN_USER_EMAIL]):
 SECRET_KEY = config("DJANGO_SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DJANGO_DEBUG", cast=bool)
+DEBUG = False
 BASE_URL = config("BASE_URL", default=None)
 
 ALLOWED_HOSTS = ['*']
