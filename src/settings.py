59df9f30debdc699bd53d2dced60f813ebbf597a
Ayub Henry <110997339+ayubsoft254@users.noreply.github.com>
2025-05-14 17:21:58 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -184,16 +184,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = "static/"
-STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
-STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
 
-
-# source(s) for python manage.py collectstatic 
-STATICFILES_DIRS = [
-    STATICFILES_BASE_DIR
-]
+# Where collectstatic will gather all static files
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
