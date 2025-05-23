917b9c92ffc61334302d567690dca0afcfbc89ed
ayubsoft254 <henryoayub15@gmail.com>
2025-05-23 17:38:42 +0300

    fix: update import statements for clarity and consistency in URL routing

--- src/urls.py
+++ src/urls.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from analytics import views
+from booking.views import landing_view
+from analytics import views as analytics_views
+from contact.views import contact_submit
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('booking.urls')),
-    path('analytics/', views.analytics_dashboard_view, name='analytics_dashboard'),
+    path('analytics/', analytics_views.analytics_dashboard_view, name='analytics_dashboard'),
+    path('contact/submit/', contact_submit, name='contact_submit'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
