e046c653a19b395aab92767acb22eac67fb381ac
ayubsoft254 <henryoayub15@gmail.com>
2025-05-23 17:47:13 +0300

    fix: remove unused import for landing_view in URL routing

--- src/urls.py
+++ src/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from booking.views import landing_view
 from analytics import views as analytics_views
 from contact.views import contact_submit
 
