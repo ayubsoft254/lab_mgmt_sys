e552743f4a6da9295a1e48edf07a3b0cff8c220e
ayubsoft254 <henryoayub15@gmail.com>
2025-05-25 07:39:29 +0300

    feat: initialize Celery app and add debug task

--- src/celery.py
+++ src/celery.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
@@ -11,3 +12,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Auto-discover tasks in all installed apps
 app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
