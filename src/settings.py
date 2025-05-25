fbc32c8b7cbc8a1689e0e68a967170ef4a419ca1
ayubsoft254 <henryoayub15@gmail.com>
2025-05-25 07:30:46 +0300

    fix: remove CELERY_BROKER_URL setting from settings.py

--- src/settings.py
+++ src/settings.py
@@ -203,7 +203,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'booking.User'
 
 # Celery Settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
