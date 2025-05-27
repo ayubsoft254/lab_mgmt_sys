6ac38158798c91200544b12fc277481e4cc48676
ayubsoft254 <henryoayub15@gmail.com>
2025-05-28 00:32:44 +0300

    feat: add URL path for Terms and Conditions view

--- resources/urls.py
+++ resources/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DocumentationView
+from .views import DocumentationView, TermsConditionsView
 
 urlpatterns = [
     # Other URLs in your resources app
     path('documentation/', DocumentationView.as_view(), name='documentation'),
+    path('terms/', TermsConditionsView.as_view(), name='terms_conditions'),
 ]
