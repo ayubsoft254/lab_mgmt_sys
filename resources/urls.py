ee871ac62b409a46681039386939c44f161756ef
ayubsoft254 <henryoayub15@gmail.com>
2025-05-28 00:39:49 +0300

    feat: add Privacy Policy URL to resources app

--- resources/urls.py
+++ resources/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DocumentationView, TermsConditionsView
+from .views import DocumentationView, TermsConditionsView, PrivacyPolicyView
 
 urlpatterns = [
     # Other URLs in your resources app
     path('documentation/', DocumentationView.as_view(), name='documentation'),
     path('terms/', TermsConditionsView.as_view(), name='terms_conditions'),
+    path('privacy/', PrivacyPolicyView.as_view(), name='privacy_policy'),
 ]
