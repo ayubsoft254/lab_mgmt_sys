6a3f53fd57a3652cd514d4da4b729d87f6e67ab3
ayubsoft254 <henryoayub15@gmail.com>
2025-05-29 01:22:29 +0300

    feat: add STATICFILES_DIRS to settings for custom static file directory

--- src/settings.py
+++ src/settings.py
@@ -188,6 +188,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Where collectstatic will gather all static files
 STATIC_ROOT = BASE_DIR / "staticfiles"
