d053a8e1f6a5171ee19497bb4f08ab54299c7c3f
ayubsoft254 <henryoayub15@gmail.com>
2025-05-29 02:24:50 +0300

    fix: update import statements for consistency in urls.py

--- resources/urls.py
+++ resources/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import DocumentationView, TermsConditionsView, PrivacyPolicyView
+from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     # Other URLs in your resources app
-    path('documentation/', DocumentationView.as_view(), name='documentation'),
-    path('terms/', TermsConditionsView.as_view(), name='terms_conditions'),
-    path('privacy/', PrivacyPolicyView.as_view(), name='privacy_policy'),
+    path('documentation/', views.DocumentationView.as_view(), name='documentation'),
+    path('terms/', views.TermsConditionsView.as_view(), name='terms_conditions'),
+    path('privacy/', views.PrivacyPolicyView.as_view(), name='privacy_policy'),
+    path('feedback/', views.FeedbackFormView.as_view(), name='feedback_form'),
+    path('feedback/success/', TemplateView.as_view(template_name='feedback_success.html'), 
+         name='feedback_success'),
+    path('api/feedback/submit/', views.ajax_feedback_submit, name='ajax_feedback_submit'),
 ]
