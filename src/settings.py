7e2eedbf7f30d24b743d8eeddf599bf1ea991513
ayubsoft254 <henryoayub15@gmail.com>
2025-08-07 00:34:15 +0300

    feat: add email admissions configuration for handling user admissions

--- src/settings.py
+++ src/settings.py
@@ -25,6 +25,9 @@ EMAIL_HOST = config("EMAIL_HOST", cast=str, default="smtp.gmail.com")
 EMAIL_PORT = config("EMAIL_PORT", cast=str, default="587") # Recommended
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str, default=None)
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default=None)
+EMAIL_HOST_USER_ADMISSIONS = config("EMAIL_HOST_USER_ADMISSIONS", cast=str, default=None)
+EMAIL_HOST_PASSWORD_ADMISSIONS = config("EMAIL_HOST_PASSWORD_ADMISSIONS", cast=str, default=None)
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=False)  # Use EMAIL_PORT 465 for SSL
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)  # Use EMAIL_PORT 587 for TLS
 EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=False)  # Use MAIL_PORT 465 for SSL
 ADMIN_USER_NAME=config("ADMIN_USER_NAME", default="Admin user")
